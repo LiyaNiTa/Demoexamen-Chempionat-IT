cf9a42c43ba12a41846298238e129150f9782326
Julia-Zhirnova <52165649+Julia-Zhirnova@users.noreply.github.com>
2024-07-09 18:29:08 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Демоэкзамен по веб-разработке'
+copyright = '2024, JV&LN'
+author = ''JV&LN'
 
 release = '0.1'
 version = '0.1.0'
